1fe4d9d8d1d09a26b6e37fe9d6b95e93ba0c60ce
VolarWang556 <131576195+VolarWang556@users.noreply.github.com>
2024-04-05 13:34:14 +1100

    Add files via upload

--- model2.py
+++ model2.py
@@ -9,6 +9,7 @@ class module(object):
             self.initial = False
             self.update_index = 0
             self.his_grad = []
+            self.training = True
 
         def update(self, value_lst, status=1):
             if self.initial is False:
@@ -26,6 +27,7 @@ class module(object):
                     self.parameters.append(value_lst)
                     self.layer_type.append(2)
                     self.update_index += 1
+                    self.his_grad.append([0, 0])
                 elif status == 0:
                     self.parameters.append(value_lst)
                     self.layer_type.append(0)
@@ -44,7 +46,7 @@ class module(object):
                     pass
                 else:
                     self.update_index += 1
-
+                    
     class Linear(Parameters):
         def __init__(self, input_size, output_size, bias=True, mode="kaiming_normal"):
             # Initialization function, defining the linear transformation parameters
@@ -111,78 +113,18 @@ class module(object):
                 super().update([x, output], 0)
                 return output
 
-    class CrossEntropyLoss(Parameters):
-
-        def __init__(self, weight=None, reduction='mean', label_smoothing=0.0):
-            # Initialization function, defining parameters of the loss function
-            self.weight = weight
-            self.reduction = reduction
-            self.label_smoothing = label_smoothing
-            # self.grad = 0  # "grad" is used to calculate the gradient. It is a gradient matrix.
-
-        def __call__(self, output, target):
-            # Callable object, calls the forward function
-            return self.forward(output, target)
-
-        def forward(self, output, target):
-            # Forward propagation function, computes the cross-entropy loss
-            if self.label_smoothing == 0:
-                # If label smoothing parameter is 0, no label smoothing is performed
-                matrix = np.zeros((len(target), target.max()))
-                for i in range(0, len(target)):
-                    matrix[i, int(target[i][0])] = 1
-
-            elif 0 < self.label_smoothing <= 1:
-                # If label smoothing parameter is between (0,1], perform label smoothing
-                matrix = np.full((len(target), target.max() + 1), self.label_smoothing / (target.max() + 1))
-                for i in range(0, len(target)):
-                    matrix[i, int(target[i][0])] = (1 - self.label_smoothing) * 1 + self.label_smoothing / (
-                            target.max() + 1)
-            else:
-                raise ValueError("Label_smoothing value can only dropped in [0,1]")
-
-            softmax = module.Softmax()  # Create Softmax object
-            output = softmax(output)  # Apply Softmax to the output
-            if self.weight is None:
-                target = matrix  # If weight is not set, use the label matrix
-            else:
-                target = self.weight * matrix  # Adjust the label matrix using weights
-            # self.grad = target - target / output  # Compute gradient
-            super().update([output, target], -1)
-            output2 = -np.log2(output)  # Perform logarithmic transformation on the Softmax output
-            loss_vec = np.sum(target * output2, axis=1)  # Compute the loss vector for each sample
-            if self.reduction == "mean":
-                loss = np.sum(loss_vec) / len(loss_vec)  # Compute mean loss
-                return loss
-            elif self.reduction == "sum":
-                loss = np.sum(loss_vec)  # Compute total loss
-                return loss
-            elif self.reduction == "none":
-                return loss_vec  # Return the loss vector for each sample
-            else:
-                raise TypeError("Reduction input is wrong. You can only input 'mean', 'sum' or 'none'.")
-
-    class Softmax:
-        def __init__(self, dim=None):
-            self.dim = dim
-
-        def __call__(self, x):
-            return self.forward(x)
-
-        def forward(self, x):
-            if self.dim is None:
-                exp = np.exp(x)
-                return exp / np.sum(exp, axis=1, keepdims=True)
-
     class BatchNormalization(Parameters):
-        def __init__(self, num_features, eps=1e-05, momentum=0.1, tracking_running_stats=True):
+        def __init__(self, num_features, eps=1e-05, momentum=0.1, tracking_running_stats=True, affine=True):
             self.num_features = num_features
             self.eps = eps
             self.momentum = momentum
             self.tracking_running_stats = tracking_running_stats
+            self.affine = affine
+            if affine is True:
+                self.gamma = None
+                self.beta = None
+                self.bn_initial = False
             # This means that we will use gamma and beta, y = gamma*X+beta
-            self.gamma = 1
-            self.beta = 0
             if tracking_running_stats:
                 self.global_mean = 0
                 self.global_variance = 0
@@ -191,21 +133,131 @@ class module(object):
             x_ = x
             return self.forward(x_)
 
-        def forward(self, x):
-            x_mean = np.mean(x, axis=0).reshape(1, -1)
-            x_var = np.var(x, axis=0).reshape(1, -1)
-            value_lst = []
-            if self.tracking_running_stats:
-                self.global_mean = (1 - self.momentum) * self.global_mean + self.momentum * self.global_mean
-                self.global_variance = (1 - self.momentum) * self.global_variance + self.momentum * self.global_variance
-                value_lst.append(self.global_mean)
-                value_lst.append(self.global_variance)
-            x_ = x - x_mean/np.sqrt(x_var+self.eps)
-            output = self.gamma * x_ + self.beta
-            value_lst.append(self.gamma)
-            value_lst.append(self.beta)
-            super().update(value_lst, 2)
-            return output
+        def forward(self, x):  # 目前只接受Batchnormalization放到第一层
+            if self.training is True:
+                x_mean = np.mean(x, axis=0).reshape(1, -1)  # 计算每列的均值和方差
+                x_var = np.var(x, axis=0).reshape(1, -1)
+                value_lst = []
+                if self.tracking_running_stats:
+                    self.global_mean = (1 - self.momentum) * self.global_mean + self.momentum * x_mean
+                    self.global_variance = (1 - self.momentum) * self.global_variance + self.momentum * x_var
+                denominator = np.sqrt(x_var + self.eps)
+                numerator = x - x_mean
+                x_ = numerator / denominator
+                if self.affine is True:
+                    if self.bn_initial is False:  # gamma和Beta的初始化过程，因为维度与x的列数有关，故放在第一次forward里面
+                        self.gamma = np.ones((1, x.shape[1]))
+                        self.beta = np.zeros((1, x.shape[1]))
+                        value_lst.append(self.gamma)
+                        value_lst.append(self.beta)
+                        grad = (denominator - (1 / len(x)) * denominator - numerator ** 2 / denominator / len(
+                            x)) / denominator ** 2
+                        grad = np.tile(self.gamma, (len(x), 1)) * grad
+                        value_lst.append(grad)
+                        value_lst.append(x_)
+                        super().update(value_lst, 2)
+                        self.bn_initial = True
+                    else:
+                        self.gamma = super().parameters[self.update_index][0]
+                        self.beta = super().parameters[self.update_index][1]
+                    output = self.gamma * x_ + self.beta
+                    return output
+                else:
+                    grad = (denominator - (1 / len(x)) * denominator - numerator ** 2 / denominator / len(
+                        x)) / denominator ** 2
+                    self.initial = True
+                    value_lst.append(grad)
+                    return x_
+            else:
+                if self.tracking_running_stats is True:
+                    if self.affine is True:
+                        x_ = (x - self.global_mean) / np.sqrt(self.global_variance + self.eps)
+                        output = self.gamma * x_ + self.beta
+                        return output
+                    else:
+                        output = (x - self.global_mean) / np.sqrt(self.global_variance + self.eps)
+                        return output
+                else:
+                    if self.affine is True:
+                        x_mean = np.mean(x, axis=0).reshape(1, -1)  # 计算每列的均值和方差
+                        x_var = np.var(x, axis=0).reshape(1, -1)
+                        denominator = np.sqrt(x_var + self.eps)
+                        numerator = x - x_mean
+                        x_ = numerator / denominator
+                        output = self.gamma * x_ + self.beta
+                        return output
+                    else:
+                        x_mean = np.mean(x, axis=0).reshape(1, -1)  # 计算每列的均值和方差
+                        x_var = np.var(x, axis=0).reshape(1, -1)
+                        denominator = np.sqrt(x_var + self.eps)
+                        numerator = x - x_mean
+                        output = numerator / denominator
+                        return output
+
+
+class CrossEntropyLoss(module.Parameters):
+
+    def __init__(self, weight=None, reduction='mean', label_smoothing=0.0):
+        # Initialization function, defining parameters of the loss function
+        self.weight = weight
+        self.reduction = reduction
+        self.label_smoothing = label_smoothing
+        # self.grad = 0  # "grad" is used to calculate the gradient. It is a gradient matrix.
+
+    def __call__(self, output, target):
+        # Callable object, calls the forward function
+        return self.forward(output, target)
+
+    def forward(self, output, target):
+        # Forward propagation function, computes the cross-entropy loss
+        if self.label_smoothing == 0:
+            # If label smoothing parameter is 0, no label smoothing is performed
+            matrix = np.zeros((len(target), target.max()))
+            for i in range(0, len(target)):
+                matrix[i, int(target[i][0])] = 1
+
+        elif 0 < self.label_smoothing <= 1:
+            # If label smoothing parameter is between (0,1], perform label smoothing
+            matrix = np.full((len(target), target.max() + 1), self.label_smoothing / (target.max() + 1))
+            for i in range(0, len(target)):
+                matrix[i, int(target[i][0])] = (1 - self.label_smoothing) * 1 + self.label_smoothing / (
+                        target.max() + 1)
+        else:
+            raise ValueError("Label_smoothing value can only dropped in [0,1]")
+
+        softmax = Softmax()  # Create Softmax object
+        output = softmax(output)  # Apply Softmax to the output
+        if self.weight is None:
+            target = matrix  # If weight is not set, use the label matrix
+        else:
+            target = np.repeat(self.weight, len(matrix), axis=1) * matrix  # Adjust the label matrix using weights
+        # self.grad = target - target / output  # Compute gradient
+        super().update([output, target], -1)
+        output2 = -np.log2(output)  # Perform logarithmic transformation on the Softmax output
+        loss_vec = np.sum(target * output2, axis=1)  # Compute the loss vector for each sample
+        if self.reduction == "mean":
+            loss = np.sum(loss_vec) / len(loss_vec)  # Compute mean loss
+            return loss
+        elif self.reduction == "sum":
+            loss = np.sum(loss_vec)  # Compute total loss
+            return loss
+        elif self.reduction == "none":
+            return loss_vec  # Return the loss vector for each sample
+        else:
+            raise TypeError("Reduction input is wrong. You can only input 'mean', 'sum' or 'none'.")
+
+
+class Softmax:
+    def __init__(self, dim=None):
+        self.dim = dim
+
+    def __call__(self, x):
+        return self.forward(x)
+
+    def forward(self, x):
+        if self.dim is None:
+            exp = np.exp(x)
+            return exp / np.sum(exp, axis=1, keepdims=True)
 
 
 class Optim(object):
@@ -220,7 +272,7 @@ class Optim(object):
                 if self.layer_type[self.update_index] == -1:  # -1 代表损失函数
                     output = self.parameters[self.update_index][0]
                     target = self.parameters[self.update_index][1]
-                    grad *= target - target / output
+                    grad *= -target + output
                     self.update_index -= 1
                 elif self.layer_type[self.update_index] == 0:  # 0 意味着激活函数ReLU
                     output = np.zeros_like(self.parameters[self.update_index][1])
@@ -229,15 +281,29 @@ class Optim(object):
                     self.update_index -= 1
                 elif self.layer_type[self.update_index] == 1:  # 1 代表全连接层
                     input_value = self.parameters[self.update_index][0]
+                    output_value = self.parameters[self.update_index][2]
+                    weight_value = self.parameters[self.update_index][1]
                     # update the weight
-                    vt = self.his_grad[self.update_index] * self.momentum + self.learning_rate * input_value.T @ grad
+                    vt = self.his_grad[self.update_index] * self.momentum + self.learning_rate * (input_value.T @ grad)
                     weight = self.parameters[self.update_index][1] - vt
-                    super().update([self.parameters[self.update_index][0], weight,
-                                    self.parameters[self.update_index][2]])
+                    super().update([input_value, weight,
+                                    output_value])
                     self.his_grad[self.update_index] = vt
+                    grad = grad @ weight_value.T
                     self.update_index -= 1
-                elif self.layer_type[self.update_index] == 2:  #代表batchnorm层
-                    parameters = self.parameters[self.update_index]
-                    if len(parameters) == 2:
-
+                elif self.layer_type[self.update_index] == 2:  # 代表batchnorm层
+                    parameters = self.parameters[self.update_index]  # gamma, beta, grad, x_ or grad
+                    if len(parameters) == 1:  # self.affine = False
+                        grad *= parameters[0]
+                        self.update_index -= -1
+                    else:
+                        gamma_his = self.his_grad[self.update_index][0]
+                        beta_his = self.his_grad[self.update_index][1]
+                        gamma_grad = self.momentum * gamma_his + self.learning_rate * \
+                                     np.sum(grad * parameters[3], axis=0)
+                        beta_grad = self.momentum * beta_his + self.learning_rate * np.sum(grad, axis=0)
+                        new_gamma = parameters[0] - gamma_grad
+                        new_beta = parameters[1] - beta_grad
+                        super().update([new_gamma, new_beta, parameters[2], parameters[3]], 2)
+                        grad *= parameters[2]
             self.update_index = 0
